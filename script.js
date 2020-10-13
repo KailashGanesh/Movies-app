a1aaa70bcc428d3ca8967d9e71f33b521adfad48
Kailash <58307685+KailashGanesh@users.noreply.github.com>
2020-10-13 19:53:04 +0530

    refactor display function

--- script.js
+++ script.js
@@ -1,95 +1,88 @@
 const inputTextBox = document.querySelector("#textbox");
 const submitBtn = document.querySelector("#submit-btn");
 const output = document.querySelector("#output");
-const baseUrl = "https://api.tvmaze.com/search/shows?q=";
 const overlay = document.querySelector("#overlay");
+
+const baseUrl = "https://api.tvmaze.com/search/shows?q="; // tvmaze
+
 const API_KEY = '?api_key=a2df3d1a7611194432bbdf1fc80540f2';
 const BASEURL = `https://api.themoviedb.org/3/movie/popular${API_KEY}&language=en-US&page=1`
+const SEARCHURL = `https://api.themoviedb.org/3/search/movie${API_KEY}&language=en-US&include_adult=false&query=`
 const IMGPATH = 'https://image.tmdb.org/t/p/w500'
 
 submitBtn.addEventListener("click", function (){
     inputText = inputTextBox.value;
-    getData(inputText);
+    searchForData(inputText);
 });
 
-function getData(search){
-    search = search.replace(' ','%20');
-    let url = baseUrl + search; // convert spaces into %20
-    let myFetch = fetch(url);
-
-    myFetch.then(function(response){
+function searchForData(search){
+    search = search.replace(' ','%20'); // convert spaces into %20
+    let url = SEARCHURL + search;
+    fetch(url).then(function(response){
         response.json().then(function (json){
-            displaySearch(json);
+            displayResults(json);
         });
     });
 
 }
 
-
-function displaySearch(json){
-    output.innerHTML =""
-    for (let i = 0; i < json.length; i++){
-        let para = document.createElement('p');
-        console.log(json[i]['show']['name']);
-        para.textContent += json[i].show.name;
-
-        let image = document.createElement('img');
-        image.src = json[i].show.image.medium
-        para.append(image)
-        output.append(para)
-    }
-}
-
 function homePage(){
     let moviesFetch = fetch(BASEURL);
-
     moviesFetch.then(function(response){
         response.json().then(function (json){
-            json.results.forEach(popMovie => {
-
-                let card_div = document.createElement('div')
-                let card = `<div class="poster">
-                    <img src="${IMGPATH + popMovie.poster_path}" alt="${popMovie.title}">
-                    </div>
-                    <div class="card-info">
-                    <h5>${popMovie.title}</h5>
-                    <span>${popMovie.vote_average}</span>
-                </div>`
-                card_div.innerHTML += card;
-                card_div.classList.add('card')
-                output.append(card_div); 
-                card_div.addEventListener('click', function(){displayInfo(popMovie)})
-            });
+            displayResults(json);
         });
     })
 }
 
+function displayResults(json){
+    output.innerHTML =""
+    console.log(json)
+    json.results.forEach(popMovie => {
+        if (popMovie.poster_path){
+            let card_div = document.createElement('div')
+            let card = `<div class="poster">
+                <img src="${IMGPATH + popMovie.poster_path}" alt="${popMovie.title}">
+                </div>
+                <div class="card-info">
+                <h5>${popMovie.title}</h5>
+                <span>${popMovie.vote_average}</span>
+            </div>`
+            card_div.innerHTML += card;
+            card_div.classList.add('card')
+            output.append(card_div); 
+            card_div.addEventListener('click', function(){displayInfo(popMovie)})
+        }
+    });
+}
+
+
+
 // overlay display info function
 function displayInfo(Data){
-    overlay.classList.add('show');
-    overlay.classList.add('background-blur')
-    // overlay.innerHTML ='';
-    let rating = Math.round(Data.vote_average/2);
+    overlay.classList.add('show');  // if this function is called then we need to display the overlay
+    overlay.classList.add('background-blur') // set's overlay background to transprent and blur
+    let rating = Math.round(Data.vote_average/2); //calculate rating out of 5 from out of 10
     let star = "";
-    let titleSize;
+    let titleSize; // this is used to reduce title font size if title is longer then 16 charters
+    // check if the veriable rating is available
     if (rating){
-        for (let i=0;i<rating;i++){
+        for (let i=0;i<rating;i++){ // rating = 3 then 3 filled-stars will be placed
             star +=`<i class="fa fa-star" aria-hidden="true"></i>`
-            console.log('star')
         }
-        if(rating < 5){
+        if(rating < 5){ // place empty stars for the rest
             for (let i=0; i<(5-rating); i++){
-                star+=`<i class="fa fa-star-o" aria-hidden="true"></i>`
+                star+=`<i class="far fa-star"></i>`
             }
         }
-    }else if (rating === 0){
+    }else if (rating === 0){ // if rating is zero then place 5 empty stars
         for (let i=0; i<5; i++){
-            star+=`<i class="fa fa-star-o" aria-hidden="true"></i>`
+            star+=`<i class="far fa-star"></i>`
         }
     }
 
     if(Data.title.length < 16){
-        titleSize = `not-longer-then-16`
+        titleSize = `not-longer-then-16` // css-class which makes the font big
     }
     overlay.innerHTML = `<span class="close-btn" onclick="overlayClose()">&times;</span>
     <div class="movie-poster">
@@ -141,7 +134,6 @@ function displayInfo(Data){
 
     // the cast cards with images
     let movieCastFetch = fetch(castUrl)
-    console.log(Data.id)
     movieCastFetch.then(function(response){
         response.json().then(function (json){
             json.cast.forEach(Cast => {
@@ -154,7 +146,6 @@ function displayInfo(Data){
                             <h4>${Cast.name}</h4>
                             <span>${Cast.character}</span>
                         </div>`
-                    console.log("doing")
                     castDiv.append(castCard);
                 }
             });
@@ -177,16 +168,16 @@ function displayInfo(Data){
         clearInterval(scrollRightInterval);
     });
     function startScrollRight(){
-        console.log("scroll right")
         castDiv.scrollLeft += 700;
     }
 
 }
 
 function overlayClose(){
+    // when this function is called it closes the overlay
     overlay.classList.remove('show')
     overlay.classList.remove('background-blur')
 }
 
 
-homePage();
+homePage(); //calls homepage functin to display the home page
